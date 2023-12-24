8b4f55a772b082807e7aebdf168de2b50b93f439
Dương Thiện Chí <thienchidtc@gmail.com>
2023-12-24 15:28:46 +0700

    CLIENT SERVER SOCKET
    Screen + mouse + keyboard

---
+++ client.py
@@ -0,0 +1,174 @@
+import socket
+import pyautogui
+from threading import Thread
+import threading
+import cv2
+import struct
+from pynput import keyboard
+import pickle
+import tkinter.messagebox as mess
+import numpy as np
+import os
+import time
+class Client:
+    def __init__(self):
+        self.host=""
+        self.my_host = socket.gethostbyname(socket.gethostname())
+        self.port = 5555
+        self.click_count = 0
+        self.client_socket = None
+        self.window = "APPSocket"
+        self.focus_window = False
+    def send_size_window(self):  #goi kich thuoc cua so
+        width= input("Choose size width of window: ")
+        height= input("Choose size height of window: ")
+        try:
+            client_host = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            client_host.connect((self.host,self.port))
+            client_host.send(f"{width} {height}".encode("utf-8"))
+            client_host.close()
+        except:
+            mess.showerror(title="Lỗi",
+                             message="Client Kết nối thất bại!")
+            os.system("taskkill /f /im "+filename)
+    def send_client_ip(self): #goi dia chi ip client
+        try:
+            client_host = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            client_host.connect((self.host,self.port))
+            client_host.send(self.my_host.encode("utf-8"))
+            client_host.close()
+        except:
+            mess.showerror(title="Lỗi",
+                             message="Client Kết nối thất bại!")
+            os.system("taskkill /f /im "+filename)
+    def send_mouse(self,data): #gởi dữ liệu chuột
+        message =  {
+            'type_data': 'mouse',
+            'data': data
+        }
+        mess = pickle.dumps(message)
+        packet = struct.pack('Q',len(mess)) + mess
+        self.client_socket.sendall(packet)
+    def mouse_event(self,event, x, y, flags,param): #Lắng nghe chuột
+            if event == cv2.EVENT_MOUSEMOVE:
+                message=f'mouse_move {x} {y}'
+                self.send_mouse(message)
+            elif event == cv2.EVENT_LBUTTONDOWN:
+                self.click_count = 1
+                message = f'left_click_and_hold {x} {y}'
+                self.send_mouse(message)
+            elif event == cv2.EVENT_LBUTTONUP:
+                if self.click_count == 1:
+                    message = f'left_release {x} {y}' 
+                    self.send_mouse(message)
+                    self.click_count = 0
+            elif event == cv2.EVENT_LBUTTONDBLCLK:
+                message = f'left_double_click {x} {y}'
+                self.send_mouse(message)
+            elif event == cv2.EVENT_RBUTTONDOWN:
+                message = f'right_click {x} {y}'
+                self.send_mouse(message)
+            elif event == cv2.EVENT_MBUTTONDOWN:
+                message = f'middle_click {x} {y}'
+                self.send_mouse(message)
+            elif event == cv2.EVENT_MOUSEWHEEL:
+                if flags > 0:
+                   self.send_mouse('scroll up')
+                else:
+                    self.send_mouse('scroll down')
+                    #client
+
+    def send_key(self,key):
+        message = pickle.dumps(key)
+        packet = struct.pack('Q',len(message)) + message
+        self.client_socket.sendall(packet)
+        
+    def on_press(self,key):
+        data = {
+            'type_data' : 'key',
+            'type' : 'down',
+            'key' : key
+        }
+        if(self.focus_window):
+            print(data['key'])
+            self.send_key(data)
+        
+    def on_release(self,key):
+        data = {
+            'type_data' : 'key',
+            'type' : 'up',
+            'key' : key
+        }
+        if(self.focus_window):
+            self.send_key(data)
+    def listen_keyboard(self):
+        while True:
+            with keyboard.Listener(on_press=self.on_press, on_release=self.on_release) as listener:
+                listener.join()
+    def revceive_screen(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #nhận màn hình
+        server_socket.bind((self.my_host, self.port))
+        server_socket.listen()
+        connection, address = server_socket.accept()
+        payload_size = struct.calcsize('>L')
+        data = b""
+        while True:
+            try:
+                break_loop = False
+                while len(data) < payload_size:
+                    received = connection.recv(4096)
+                    if received == b'':
+                        connection.close()
+                        break_loop = True
+                        break
+                    data += received
+                if break_loop:
+                    break
+                packed_msg_size = data[:payload_size]
+                
+                data = data[payload_size:]
+                
+                msg_size = struct.unpack(">L", packed_msg_size)[0]
+                while len(data) < msg_size:
+                    data += connection.recv(4096)
+                    
+                frame_data = data[:msg_size]
+                data = data[msg_size:]
+                
+                frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+                frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+                cv2.namedWindow(self.window, cv2.WINDOW_NORMAL)
+                cv2.resizeWindow(self.window,frame.shape[1], frame.shape[0])
+                
+                cv2.imshow(self.window, frame) #show màn hình
+                if (pyautogui.getActiveWindowTitle() == self.window):
+                    self.focus_window = True
+                else:
+                    self.focus_window = False
+                cv2.setMouseCallback(self.window, self.mouse_event)  #gởi chuột
+                cv2.waitKey(1)
+            except:
+                connection.close()
+                cv2.destroyAllWindows()
+                break
+            # if cv2.waitKey(1): 
+            #     connection.close()
+            #     cv2.destroyAllWindows()
+            #     break
+    def start_client(self):
+        self.host=input("Enter host: ")
+        self.send_client_ip()
+        self.send_size_window()
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #dùng UDP để gởi chuột
+        self.client_socket.connect((self.host, self.port))
+        t1=threading.Thread(target=self.revceive_screen) #nhận màn hình
+        t2= threading.Thread(target=self.listen_keyboard) 
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+        self.client_socket.close()
+if __name__ == "__main__":
+    client = Client()
+    client.start_client()
+    
--- server.py
+++ server.py
@@ -1,47 +1,142 @@
 import socket
-import os
-import mss,sys
+import cv2
+import pynput
+from pynput.mouse import Button as but, Controller
+import numpy as np
+import threading
+import pyautogui
+import keyboard 
+import win32api
+import pickle
 import tkinter.messagebox as mess
-from threading import Thread
-local = sys.argv[0]
-local = local.split("\\")
-filename = local[len(local)-1]
-
-
-
+import os
+import struct
+from pynput import keyboard
 class Server:
     def __init__(self):
+        self.x_res,self.y_res = int(pyautogui.size()[0]), int(pyautogui.size()[1])
+        self.width,self.height = int(pyautogui.size()[0]), int(pyautogui.size()[1])
+        self.host =""
+        self.my_host = socket.gethostbyname(socket.gethostname())
+        self.port =5555
+        self.server_socket = None
+        self.screen_quality = 50
         self.mouse = Controller()
-        self.my_host = self.get_ip_address()
-        self.your_host = ""
-        self.port = 5500
-        self.server_control = None
-        self.id_client = ("",0)
-        self.filename = ""
-    def get_ip_address(self):
-        my_host = socket.gethostbyname(socket.gethostname())
-        return my_host
-    def recive_client_host(self):
+    def receive_client_ip(self): #nhan dia chi ip client
         try:
             server_host = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
             server_host.bind((self.my_host, self.port))
-            self.your_host = server_host.recv(1024)
-            self.your_host = str(self.your_host.decode("utf-8"))
+            server_host.listen()
+            conn, addr = server_host.accept()
+            self.host = conn.recv(1024)
+            self.host = str(self.host.decode("utf-8"))
+            conn.close()
             server_host.close()
         except:
             mess.showerror(title="Lỗi",
-                             message="Kết nối thất bại!")
-            os.system("taskkill /f /im "+filename)
-    def recive_control(self):
-        try: 
-            server_host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server_host.bind((self.my_host, self.my_port))
+                             message="Server Kết nối thất bại!")
+    def recv_size_window(self):   #nhan kich thuoc cua so
+        try:
+            server_host = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            server_host.bind((self.my_host, self.port))
+            server_host.listen()
+            conn, addr = server_host.accept()
+            size = conn.recv(1024).decode("utf-8")
+            size = size.split(' ')
+            self.width,self.height = size[0],size[1]
+            conn.close()
+            server_host.close()
         except:
             mess.showerror(title="Lỗi",
-                             message="Kết nối thất bại!")
-            os.system("taskkill /f /im "+filename)
+                             message="Server Kết nối thất bại!")
             
-if __name__ == '__main__':
-         
-        
-
+    def get_frame(self):  #chụp màn hình
+        screen = pyautogui.screenshot()
+        frame = np.array(screen)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, (self.x_res,self.y_res), interpolation=cv2.INTER_AREA)
+        return frame
+    def send_display(self):   #gởi màn hình
+        socket_screen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #dùng TCP để gởi màn hình
+        socket_screen.connect((self.host,self.port))
+        while True:
+            frame = self.get_frame()
+            result, frame = cv2.imencode('.jpg', frame,[int(cv2.IMWRITE_JPEG_QUALITY), self.screen_quality])
+            data = pickle.dumps(frame, 0)
+            size = len(data)
+            try:
+                socket_screen.sendall(struct.pack('>L', size) + data)
+            except ConnectionResetError:
+                print("Connection reset failed")
+                break
+            except ConnectionAbortedError:
+                print("Connection aborted")
+                break
+            except BrokenPipeError:
+                print("Connection failed")
+                break
+    def get_keyboard(self,key):
+        controller = keyboard.Controller()
+        if key['type'] == 'down':
+            controller.press(key['key'])
+        else:
+            controller.release(key['key'])
+    def recv_control(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #dùng UDP để nhận chuột
+        self.server_socket.bind((self.my_host,self.port))
+    # client_controll.listen(1)
+    # print("Listening......")
+    # conn, adrr = client_controll.accept()
+        hold=0
+        header = struct.calcsize('Q')
+        data=b''
+        while True:
+            while len(data) < header:
+                data += self.server_socket.recv(1024)
+            mess_size = struct.unpack('Q',data[:header])[0]
+            data = data[header:]
+            while len(data) < mess_size:
+                data += self.server_socket.recv(1024)
+            resquest = data[:mess_size]
+            data = data[mess_size:]
+            key = pickle.loads(resquest)
+            if(key['type_data'] == 'mouse'):
+                datagram = key['data']
+                datagram = datagram.split()
+                if(len(datagram) == 3):
+                    x,y = int (int(datagram[1])*self.x_res/self.width), int(int(datagram[2])*self.y_res/self.height)
+                    if(datagram[0] == 'mouse_move'):
+                        win32api.SetCursorPos((x,y))
+                    elif(datagram[0] == 'left_click_and_hold' and hold==0):
+                        pyautogui.mouseDown()
+                        hold = 1
+                    elif(datagram[0] == 'left_release' and hold ==1):
+                        pyautogui.mouseUp()
+                        hold=0
+                    elif(datagram[0] == 'left_double_click'):
+                        pyautogui.doubleClick()
+                    elif(datagram[0] == 'right_click'):
+                        pyautogui.rightClick()
+                    elif(datagram[0] == 'middle_click'):
+                        pyautogui.middleClick()
+                elif(len(datagram) == 2):
+                    if(datagram[0] == 'scroll' and datagram[1] == 'up'):
+                        self.mouse.scroll(0,1)
+                    elif(datagram[0] == 'scroll' and datagram[1] == 'down'):
+                        self.mouse.scroll(0,-1)    
+            else:
+                self.get_keyboard(key)    
+    def start_server(self):
+        self.receive_client_ip()
+        self.recv_size_window ()
+        t2= threading.Thread(target=self.recv_control) #nhận chuột va phim
+        t1= threading.Thread(target=self.send_display) #gởi màn hình
+        t1.daemon = True
+        t2.daemon = True
+        t2.start()
+        t1.start()
+        t2.join()
+        t1.join()
+if __name__ == "__main__":
+    server_run = Server()
+    server_run.start_server()
